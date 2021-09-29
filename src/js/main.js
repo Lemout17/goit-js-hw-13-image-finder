73d9329e0656f6785e33cf964a6bb314ce73ef40
Vlad Safin <djutsu17@gmail.com>
2021-09-29 14:59:04 +0300

    add css styles for button and img

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,11 @@ import errorRequest from './pnotify';
 const basicLightbox = require('basiclightbox');
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-
-
 const refs = {
-    container: document.querySelector('.gallery'),
-    form: document.querySelector('.search-form'),
-    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-
-}
+  container: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
 const apiService = new ApiService();
 
 refs.form.addEventListener('submit', onSearch);
@@ -19,42 +16,41 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 refs.loadMoreBtn.classList.add('is-hidden');
 
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    apiService.query = e.currentTarget.elements.query.value;
-    apiService.resetPage();
-    
-    if (e.currentTarget.elements.query.value === '') {
-        errorRequest();
-        return;
-    };
-    
-    apiService.getFetch().then(hits => {
-        
-        clearImagesContainer();
-        appendImagesMarkup(hits);
-        refs.loadMoreBtn.classList.remove('is-hidden');
-    });
+  apiService.query = e.currentTarget.elements.query.value;
+  apiService.resetPage();
+
+  if (e.currentTarget.elements.query.value === '') {
+    errorRequest();
+    return;
+  }
+
+  apiService.getFetch().then(hits => {
+    clearImagesContainer();
+    appendImagesMarkup(hits);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  });
 }
 
 function onLoadMore() {
-    apiService.getFetch().then(appendImagesMarkup);
+  apiService.getFetch().then(appendImagesMarkup);
 }
 
 function appendImagesMarkup(hits) {
-    refs.container.insertAdjacentHTML('beforeend',imageTmpl(hits))
+  refs.container.insertAdjacentHTML('beforeend', imageTmpl(hits));
 }
 
 function clearImagesContainer() {
-    refs.container.innerHTML = '';
+  refs.container.innerHTML = '';
 }
 
-
-refs.container.addEventListener('click', (e) =>
-{
+refs.container.addEventListener('click', e => {
   e.preventDefault();
   if (e.target.nodeName !== 'IMG') {
     return;
   }
-  basicLightbox.create(`<img class="lightbox__image" src='${e.target.dataset.source}'>`,).show();
-})
+  basicLightbox
+    .create(`<img class="lightbox__image" src='${e.target.dataset.source}'>`)
+    .show();
+});
